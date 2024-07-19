a338e145804efa5f9823e3ac05634ebbacc0a23f
JuanLuisReyes <reyes.juanluis.perez@gmail.com>
2024-07-19 17:16:59 -0600

    Documentation applied for the functions created. Also, student names in th Excel file modified.

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ import pandas as pd
 import os
 
 def create_dictionary(subject, student):
+    '''Creates a dictionary based on a list of students and its respective teacher information
+    
+    args:
+        - subject: information for the subject and its respective teacher.
+        - student: information for the current student.
+    '''
+
     constantes = {
                 'name': subject["Nombre"], 
                 'email': subject["Correo"], 
@@ -18,20 +25,31 @@ def create_dictionary(subject, student):
     return constantes
 
 def create_file(constantes, subject, student):
+    '''Creates the docx file with the information obtained from the Excel file.
+    
+    args:
+        - constantes: Dictionary with the information to be passed to the docx file.
+        - subject: Information to rename the new file (subject name).
+        - student: Information to rename the new file (student name).
+
+    '''
     doc.render(constantes)
     doc.save(f"Calificaciones/{subject["Materia"]}/Calificaiones_{student["Nombre"]}.docx")
 
 def create_folders(project_file_path):
-    os.mkdir(project_file_path)
-
-def create_scores_folder(scores_folder_path):
-    if not os.path.exists(scores_folder_path):
-        os.mkdir(scores_folder_path)
+    '''Creates the folder for the current subject if not exists
+    
+    args:
+        - project_file_path: Current folder path to be created.
+    
+    '''
+    if not os.path.exists(project_file_path):
+        os.mkdir(project_file_path)
 
 if __name__ == "__main__":
     project_file_path = os.getcwd()
     project_file_path = os.path.join(project_file_path, "Calificaciones")
-    create_scores_folder(project_file_path)
+    create_folders(project_file_path)
     doc = DocxTemplate("scores_template.docx")
     maestro_sheet = pd.read_excel(io="students_scores.xlsx", sheet_name="Maestro")
 
